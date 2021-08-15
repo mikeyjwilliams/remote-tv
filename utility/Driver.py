2ea75f81a08eb540d0969a3ad33efc67103e69e7
Michael Williams <williamsmikeyj@outlook.com>
2021-08-15 15:01:50 -0400

    update driver file

--- utility/Driver.py
+++ utility/Driver.py
@@ -31,7 +31,7 @@ class Driver():
         if browser == 'chrome':
             chrome_option = self.driver.ChromeOptions()
             chrome_user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36'
-            chromedriver_path = '../../../drivers/chromedriver.exe'
+            
             # if chrome add to driver_option chrome options
             # chrome_option.add_argument(' - incognito')
             chrome_option.add_argument(chrome_user_agent)
