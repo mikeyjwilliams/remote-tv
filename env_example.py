275e2198c60f72e25a247ec50c43867cf534405f
Michael Williams <williamsmikeyj@outlook.com>
2021-08-16 13:25:43 -0400

    ignore .history and env folder example file of env

---
+++ env_example.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+'''
+example of environment variables that will be different for each user
+    and need to be set up individually per basis.
+1. Create a `env` folder.
+2. copy `env_example.py` to folder `env`.
+3. rename file `env_example.py` to `env.py`
+4. Fill in the blanks with info you desire to use.
+'''
