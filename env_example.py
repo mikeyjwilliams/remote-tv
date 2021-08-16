4279755f4fd8a3184616e21adca620f6bc90061e
Michael Williams <williamsmikeyj@outlook.com>
2021-08-16 13:28:59 -0400

    netflix profile added to env example

--- env_example.py
+++ env_example.py
@@ -8,3 +8,9 @@ example of environment variables that will be different for each user
 3. rename file `env_example.py` to `env.py`
 4. Fill in the blanks with info you desire to use.
 '''
+
+
+
+
+class Env():
+    NETFLIX_PROFILE = None
