51c3054d72002b4c44fdabe2abdc77595b234bf0
Mikey Williams <williamsmikeyj@outlook.com>
2021-08-16 21:23:49 -0400

    simple design

--- remote_shows.py
+++ remote_shows.py
@@ -25,15 +25,21 @@ chrome_options.add_argument('start-maximized')
 chrome_path = '../drivers/chromedriver.exe'
 driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
 
-driver.get('www.google.com')
-time.sleep(10)
-input_bar = driver.find_element_by_id('input')
-input_bar.clear()
-input_bar.send_keys('https://www.netflix.com')
+driver.get('https://www.netflix.com/login')
+
+email_login = driver.find_element_by_xpath('//*[@id="id_userLoginId"]')
+email_login.send_keys(Env.NETFLIX_USER)
+password_login = driver.find_element_by_xpath('//*[@id="id_password"]')
+password_login.send_keys(Env.NETFLIX_PASS)
+login_button = driver.find_element_by_xpath('//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button')
+login_button.send_keys(Keys.ENTER)
+
+
+
+
 time.sleep(5)
-driver.save_screenshot('front-page.png')
-driver.close()
-driver.quit()
+
+
 
 
 
