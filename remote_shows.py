d036b9b7a8d0c103cd27a4aeba29343ae1880f0e
michael williams <williamsmikeyj@outlook.com>
2021-08-19 08:08:30 -0400

    put on pause

--- remote_shows.py
+++ remote_shows.py
@@ -19,6 +19,8 @@ class TestData():
 chrome_options = Options()
 
 chrome_options.add_argument('start-maximized')
+chrome_options.add_argument('--disable-notifications')
+chrome_options.add_argument("--auto-open-devtools-for-tabs")
 
 os_system = platform.system()
 chrome_path = None
@@ -32,12 +34,35 @@ driver = webdriver.Chrome(executable_path=chrome_path, options=chrome_options)
 
 driver.get('https://www.netflix.com/login')
 
+# email user login
 email_login = driver.find_element_by_xpath('//*[@id="id_userLoginId"]')
+# env user name send input
 email_login.send_keys(Env.NETFLIX_USER)
+# password login find path
 password_login = driver.find_element_by_xpath('//*[@id="id_password"]')
+# env password send input
 password_login.send_keys(Env.NETFLIX_PASS)
+# locate login button
 login_button = driver.find_element_by_xpath('//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button')
+# click login button
 login_button.send_keys(Keys.ENTER)
+
+# locate all profiles available
+profile_links = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'profile-name')))
+# second profile selected
+second_profile = profile_links[1]
+# choose second profile to select.
+second_profile.click()
+
+icon_search = WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME,'icon-search')))
+icon_search.click()
+
+
+# search_input = WebDriverWait(driver, 20).until()
+# search_input.send_keys('Hilda')
+# search_input.send_keys(Keys.ENTER)
+
+
 
 
 
